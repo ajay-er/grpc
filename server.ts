b32c02d8e29f803b73c6b2b14e9a20809b2619bf
ajay <ajayzyan100@gmail.com>
2024-02-09 13:46:26 +0530

    server started

--- server.ts
+++ server.ts
@@ -1,6 +1,37 @@
-import path from 'path';
-import * as grpc from '@grpc/grpc-js'
-import * as protoloader from '@grpc/proto-loader'
+import path from "path";
+import * as grpc from "@grpc/grpc-js";
+import * as protoloader from "@grpc/proto-loader";
+import { ProtoGrpcType } from "./proto/random";
+import { RandomHandlers } from "./proto/randomPackage/Random";
 
-const PORT = 3000
+const PORT = 3000;
+const PROTO_FILE = "./proto/random.proto";
 
+const packageDef = protoloader.loadSync(path.resolve(__dirname, PROTO_FILE));
+const grpcObj = grpc.loadPackageDefinition(
+  packageDef
+) as unknown as ProtoGrpcType;
+const randomPackage = grpcObj.randomPackage;
+
+function main() {
+  const server = getServer();
+  server.bindAsync(`0.0.0.0:${PORT}`,grpc.ServerCredentials.createInsecure(),(err,port)=>{
+    if(err) {
+        console.error(err);
+        return 
+    }
+    console.log("Server started on PORT:",port)
+  })
+}
+
+function getServer(): grpc.Server {
+  const server = new grpc.Server();
+  server.addService(randomPackage.Random.service, {
+    PingPong: (req,res) => {
+        console.log(req,res)
+    },
+  } as RandomHandlers);
+  return server;
+}
+
+main();
